52dcc0da959e9fa7667f2a2000cd18b72ac61947
Maxime Britto <maxime@purplegiraffe.fr>
2023-03-22 00:28:13 +0100

    Added more functions to simplify reading and setting properties on Directus_data objects
    getters and setters for properties of type:
    - DirectusFile
    - DateTime
    - List<String>

--- lib/src/model/directus_data.dart
+++ lib/src/model/directus_data.dart
@@ -25,6 +25,16 @@ abstract class DirectusData {
     return idData;
   }
 
+  /// Returns the id of the item if you used `Integer` as the [id] type in Directus
+  int? get intId {
+    dynamic idData = getValue(forKey: "id");
+    if (idData is int) {
+      return idData;
+    }
+
+    return null;
+  }
+
   Map<String, dynamic> getRawData() => _rawReceivedData;
   bool get needsSaving => updatedProperties.isNotEmpty;
 
@@ -52,6 +62,66 @@ abstract class DirectusData {
       return DirectusFile.fromJSON(value);
     }
     return null;
+  }
+
+  /// Set a DirectusFile to the value of the given key
+  /// If the file is null, the value will be set to null
+  void setOptionalDirectusFile(DirectusFile? file, {required String forKey}) {
+    if (file != null) {
+      setValue(file.id, forKey: forKey);
+    } else {
+      setValue(null, forKey: forKey);
+    }
+  }
+
+  List<T> getObjectList<T>(
+      {required String forKey,
+      required T Function(Map<String, dynamic>) fromMap}) {
+    final dynamic value = getValue(forKey: forKey);
+    if (value is List<dynamic>) {
+      return value.map((e) => fromMap(e)).toList();
+    }
+    return [];
+  }
+
+  List<String> getStringList({required String forKey}) {
+    final dynamic value = getValue(forKey: forKey);
+    if (value is List<dynamic>) {
+      return value.map((e) => e.toString()).toList();
+    }
+    return [];
+  }
+
+  /// Returns a DateTime if the value is a DateTime or a String that can be parsed as a DateTime
+  /// This property can be used if you are not certain your property will be filled for every object in the collection
+  DateTime? getOptionalDateTime({required String forKey}) {
+    final dynamic value = getValue(forKey: forKey);
+    DateTime? date;
+    if (value is DateTime) {
+      date = value;
+    } else if (value is String) {
+      date = DateTime.tryParse(value);
+    }
+    return date;
+  }
+
+  /// Returns a DateTime for a given property.
+  /// Only use this property if you are certain your property will be filled for every object in the collection
+  /// If you are not certain, use [getOptionalDateTime] instead
+  DateTime getDateTime({required String forKey}) {
+    final dynamic value = getOptionalDateTime(forKey: forKey);
+    assert(value != null);
+    return value!;
+  }
+
+  /// Sets a DateTime for a given property.
+  /// if the value is null, the property will be removed from the updated properties
+  void setOptionalDateTime(DateTime? value, {required String forKey}) {
+    if (value != null) {
+      setValue(value.toIso8601String(), forKey: forKey);
+    } else {
+      setValue(null, forKey: forKey);
+    }
   }
 
   /// returns a map of all the properties of the item merged with the ones that have been updated
--- test/model/directus_data_test.dart
+++ test/model/directus_data_test.dart
@@ -1,4 +1,5 @@
 import 'package:directus_api_manager/src/model/directus_data.dart';
+import 'package:directus_api_manager/src/model/directus_file.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
@@ -60,6 +61,79 @@ main() {
       sut.setValue(null, forKey: "checkString");
 
       expect(sut.getValue(forKey: "checkString"), null);
+    });
+
+    test("getStringList should return an empty list if the property is null",
+        () {
+      final sut = TestDirectusData({"id": "abc"});
+      expect(sut.getStringList(forKey: "checkString"), []);
+    });
+
+    test(
+        "getStringList should return an empty list if the property is not a list",
+        () {
+      final sut = TestDirectusData({"id": "abc", "checkString": "star wars"});
+      expect(sut.getStringList(forKey: "checkString"), []);
+    });
+
+    test("getStringList should return a list of string", () {
+      final sut = TestDirectusData({
+        "id": "abc",
+        "checkString": ["star wars", "lord of the ring"]
+      });
+      expect(sut.getStringList(forKey: "checkString"),
+          ["star wars", "lord of the ring"]);
+    });
+
+    test("getDirectusFile should return a DirectusFile", () {
+      final sut = TestDirectusData({
+        "id": "abc",
+        "fileData": {"id": "123", "title": "star wars"}
+      });
+      final file = sut.getDirectusFile(forKey: "fileData");
+      expect(file, isNotNull);
+      expect(file, isA<DirectusFile>());
+      expect(file.id, "123");
+      expect(file.title, "star wars");
+    });
+
+    test("getDirectusFile should throw if the property is null", () {
+      final sut = TestDirectusData({"id": "abc"});
+      expect(() => sut.getDirectusFile(forKey: "fileData"),
+          throwsA(isA<AssertionError>()));
+    });
+
+    test("getDirectusFile should throw if the property is not a map", () {
+      final sut = TestDirectusData({"id": "abc", "fileData": "star wars"});
+      expect(() => sut.getDirectusFile(forKey: "fileData"),
+          throwsA(isA<AssertionError>()));
+    });
+
+    test("getOptionalDirectusFile should return null if the property is null",
+        () {
+      final sut = TestDirectusData({"id": "abc"});
+      final file = sut.getOptionalDirectusFile(forKey: "fileData");
+      expect(file, null);
+    });
+
+    test(
+        "getOptionalDirectusFile should return null if the property is not a map",
+        () {
+      final sut = TestDirectusData({"id": "abc", "fileData": "star wars"});
+      final file = sut.getOptionalDirectusFile(forKey: "fileData");
+      expect(file, null);
+    });
+
+    test("getOptionalDirectusFile should return a DirectusFile if present", () {
+      final sut = TestDirectusData({
+        "id": "abc",
+        "fileData": {"id": "123", "title": "star wars"}
+      });
+      final file = sut.getOptionalDirectusFile(forKey: "fileData");
+      expect(file, isNotNull);
+      expect(file, isA<DirectusFile?>());
+      expect(file?.id, "123");
+      expect(file?.title, "star wars");
     });
   });
 }
